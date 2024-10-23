8c77c9827a92daa94f789aac6fca0c4e1df255ba
Drroweis <qusaialroweisi1@gmail.com>
2024-10-24 02:45:20 +0300

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+// Update baseURL to use the production URL
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api', // Use an environment variable for production
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
